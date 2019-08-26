0064eb2f651aa18fd8e0dd686ba7143da877c69b
7117 <qinglangsalaheiyou@163.com>
2019-08-26 20:35:43 +0800

    视频：评论的查看添加与测试

--- src/api/dbops/api_test.go
+++ src/api/dbops/api_test.go
@@ -2,9 +2,9 @@ package dbops
 
 import (
 	"testing"
-	// "strconv"
-	// "time"
-	// "fmt"
+	"strconv"
+	"time"
+	"fmt"
 )
 
 
@@ -105,4 +105,42 @@ func testRegetVideoInfo(t *testing.T) {
 	}
 }
 
+// 测试评论
+func TestComments(t *testing.T) {
+	clearTables()
+	t.Run("AddUser", testAddUser)
+	t.Run("AddCommnets", testAddComments)
+	t.Run("ListComments", testListComments)
+}
+
+func testAddComments(t *testing.T) {
+	vid := "12345"
+	aid := 1
+	content := "I like this video"
+
+	err := AddNewComments(vid, aid, content)
+
+	if err != nil {
+		t.Errorf("Error of AddComments: %v", err)
+	}
+}
+
+func testListComments(t *testing.T) {
+	vid := "12345"
+	from := 1514764800
+	// atoi字符串转化为整型  
+	// 将整数转换为字符串形式
+	// 第一个是数字
+	// 第二个是进制
+	to, _ := strconv.Atoi(strconv.FormatInt(time.Now().UnixNano()/1000000000, 10))
+
+	res, err := ListComments(vid, from, to)
+	if err != nil {
+		t.Errorf("Error of ListComments: %v", err)
+	}
+
+	for i, ele := range res {
+		fmt.Printf("comment: %d, %v \n", i, ele)
+	}
+}	
 
