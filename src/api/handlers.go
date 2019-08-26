73c4bb21ddd9a22d41e1ca0119e7afd8c21957be
7117 <qinglangsalaheiyou@163.com>
2019-08-26 13:34:03 +0800

    项目：post方法

--- src/api/handlers.go
+++ src/api/handlers.go
@@ -6,7 +6,14 @@ import (
 	"net/http"
 )
 
+// goroutine
 func CreateUser(w http.ResponseWriter,r *http.Request,p httprouter.Params){
 	io.WriteString(w,"cretae user handle")
 }
 
+// goroutine
+func Login(w http.ResponseWriter,r *http.Request,p httprouter.Params){
+	uname:=p.ByName("user_name")
+	// w封装的conn.write
+	io.WriteString(w,uname)
+}
--- src/api/main.go
+++ src/api/main.go
@@ -10,12 +10,14 @@ func RegisterHandles()*httprouter.Router{
 	router:=httprouter.New();
 
 	router.POST("/user",CreateUser)
+	router.POST("/user/:user_name",Login)
 
 	return router;
 }
 
 func main(){
 	r:=RegisterHandles()
+	// 会堵塞  会注册r函数
 	http.ListenAndServe("127.0.0.1:8000",r)
 
 }
