fe3faaa369c43e0fae1c59ede9d6fe3fc77fe910
7117 <qinglangsalaheiyou@163.com>
2019-08-28 21:23:46 +0800

    scheduler：分析分配者消费者过程

---
+++ src/scheduler/taskrunner/defs.go
@@ -0,0 +1,22 @@
+package taskrunner
+
+const(
+	// 预支controlChan的消息
+	// controlChan发送READY_TO_DISPATCH被分配者DISPATCH收到后，分配者DISPATCH会进行下发数据到datachan里面
+	// 下发完之后，给controlchan传递执行信息，到执行者运转了
+	READY_TO_DISPATCH ="d"
+	// 数据下发完之后，通过controlchan发消息值消费者EXECUTE，消费者EXECUTE就会去读datachan的消息，进行操作
+	// 执行完之后，给controlchan传递分配信息，到分配者运转了
+	READY_TO_EXECUTE = "e"
+	// 所以controlChan是命令的传输者
+	// 所以datachan是数据的池子
+	CLOSE = "c"
+)
+// 预定义
+type controlChan chan string
+// 泛型
+type dataChan chan interface{}
+// fn函数类型的  
+// 入参为datachan  出参为error
+type fn func(dc dataChan) error
+
