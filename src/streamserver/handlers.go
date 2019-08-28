5d9d73e5284aa2ae24399ad72134599c4ff7c647
7117 <qinglangsalaheiyou@163.com>
2019-08-28 11:25:53 +0800

    sreaming：上传

--- src/streamserver/handlers.go
+++ src/streamserver/handlers.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"time"
 	"log"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -25,6 +26,7 @@ func testPageHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params
 func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	vid := p.ByName("vid-id")
 	vl := VIDEO_DIR + vid
+	fmt.Println(vl);
 
 	video, err := os.Open(vl)
 	if err != nil {
@@ -39,3 +41,43 @@ func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 	defer video.Close()
 }
 
+func uploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// 能够读取到的最大的缓冲区内容
+	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
+	// ParseMultipartForm表单的最大
+	if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+		sendErrorResponse(w, http.StatusBadRequest, "File is too big")
+		return 
+	}
+
+	// 从表单拿到文件
+	// file对应表单的name
+	// 第二个是类型
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		log.Printf("Error when try to get file: %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return 
+	}
+
+	// 读取到我们数据里面 
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("Read file error: %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+	}
+
+	// 获取文件名
+	fn := p.ByName("vid-id")
+	// 保存文件
+	err = ioutil.WriteFile(VIDEO_DIR + fn, data, 0777)
+	if err != nil {
+		log.Printf("Write file error: %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	// 反馈code与信息
+	w.WriteHeader(http.StatusCreated)
+	io.WriteString(w, "Uploaded successfully")
+}
