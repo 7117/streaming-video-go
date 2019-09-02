3262713b043b1408a10f174d0b551532e668b1e4
7117 <qinglangsalaheiyou@163.com>
2019-09-02 12:54:02 +0800

    frontend:api透传与proxy转发

--- src/webserver/web/handlers.go
+++ src/webserver/web/handlers.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"html/template"
-	// "encoding/json"
+	"encoding/json"
 	"log"
 	"net/http"
-	// "io/ioutil"
-	// "net/url"
-	// "net/http/httputil"
+	"io/ioutil"
+	"net/url"
+	"net/http/httputil"
 	"github.com/julienschmidt/httprouter"
-	// "io"
+	"io"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -17,6 +17,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	cname, err1 := r.Cookie("username")
 	sid, err2 := r.Cookie("session")
 
+	// 访客
 	// 无值的时候  没有登录的
 	if err1 != nil || err2 != nil {
 		// 需要赋值的参数
@@ -32,6 +33,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		return
 	}
 
+	// 用户
 	// 有值得时候  登陆了的
 	if len(cname.Value) != 0 && len(sid.Value) != 0 {
 		http.Redirect(w, r, "D:/Github/Streaming-video/src/webserver/template/userhome.html", http.StatusFound)
@@ -39,60 +41,72 @@ func homeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	}
 }
 
-// func userHomeHandler(w http.ResponseWriter,r *http.Request,ps httprouter.Params){
-// 	// // 获取值
-// 	// cname,err:=r.Cookie("username")	//Cookie值用于登陆过的
-// 	// fname:=r.FormValue("username")		//填充值用于未登录过的
-// 	// var p *UserPage
+func userHomeHandler(w http.ResponseWriter,r *http.Request,ps httprouter.Params){
+	// 获取值
+	cname,err:=r.Cookie("username")	//Cookie值用于登陆过的
+	fname:=r.FormValue("username")	
+	var p *UserPage
 
-// 	// if err!=nil  {
-// 	// 	http.Redirect(w,r,"/",http.StatusFound)
-// 	// 	return
-// 	// }
+	// 未登录的
+	if err!=nil  {
+		http.Redirect(w,r,"/",http.StatusFound)
+		return
+	}
 
-// 	// //Cookie值
-// 	// if len(cname.Value) !=0 {
-// 	// 	p=&UserPage{Name:cname.Value}
-// 	// //填充值
-// 	// }else if len(fname)!=0{
-// 	// 	p=&UserPage{Name:fname}
-// 	// }
+	// 登录了的
+	//Cookie值
+	if len(cname.Value) !=0 {
+		p=&UserPage{Name:cname.Value}
+	//填充值
+	}else if len(fname)!=0{
+		p=&UserPage{Name:fname}
+	}
 
-// 	// 注册页面
-// 	p:=&UserPage{Name:"aaaa"}
-// 	t,e:=template.ParseFiles("D:/Github/Streamingmedia/src/webserver/template/userhome.html")
+	// 注册页面
+	// p=&UserPage{Name:"孙潇2"}
+	t,e:=template.ParseFiles("D:/Github/Streamingmedia/src/webserver/template/userhome.html")
 
-// 	if e !=nil {
-// 		log.Printf("parsing template home.html error:%s",e)
-// 		return
-// 	}
-// 	t.Execute(w,p)
-// 	return
-// }
+	if e !=nil {
+		log.Printf("parsing template home.html error:%s",e)
+		return
+	}
+	t.Execute(w,p)
+	return
+}
 
-// func apiHandler(w http.ResponseWriter,r *http.Request,ps httprouter.Params){
-// 	// 方式
-// 	if r.Method!=http.MethodPost {
-// 		re,_:=json.Marshal(ErrorRequestNotRecognized)
-// 		io.WriteString(w,string(re))
-// 		return
-// 	}
-// 	// body
-// 	res,_:=ioutil.ReadAll(r.Body)
-// 	apibody:=&ApiBody{}
-// 	if err:=json.Unmarshal(res,apibody);err!=nil{
-// 		re,_:=json.Marshal(ErrorRequestBodyParseFailed)
-// 		io.WriteString(w,string(re))
-// 		return
-// 	}
-// 	// request
-// 	request(apibody,w,r)
+func apiHandler(w http.ResponseWriter,r *http.Request,ps httprouter.Params){
+	// 方式get  转发一般是post方式的
+	if r.Method!=http.MethodPost {
+		re,_:=json.Marshal(ErrorRequestNotRecognized)
+		io.WriteString(w,string(re))
+		return
+	}
 
-// 	defer r.Body.Close()
-// }
+	// body
+	// ioutil流失读写
+	res,_:=ioutil.ReadAll(r.Body)
+	apibody:=&ApiBody{}
+	// 转化为非json形式
+	if err:=json.Unmarshal(res,apibody);err!=nil{
+		re,_:=json.Marshal(ErrorRequestBodyParseFailed)
+		io.WriteString(w,string(re))
+		return
+	}
+	// 透传的调用
+	// request
+	// 第一个参数请求的body
+	// 第二个参数响应
+	// 第三个参数请求
+	request(apibody,w,r)
 
-// func proxyHandler(w http.ResponseWriter,r *http.Request,ps httprouter.Params){
-// 	u,_:=url.Parse("http://127.0.0.1:9000/")
-// 	proxy:=httputil.NewSingleHostReverseProxy(u)  //替换
-// 	proxy.ServeHTTP(w,r)
-// }
+	defer r.Body.Close()
+}
+
+func proxyHandler(w http.ResponseWriter,r *http.Request,ps httprouter.Params){
+	// 目标url
+	u,_:=url.Parse("http://127.0.0.1:9000/")
+	// 创建一个proxy代理（进行替换域名）
+	proxy:=httputil.NewSingleHostReverseProxy(u) 
+	// 地址进行转发
+	proxy.ServeHTTP(w,r)
+}
